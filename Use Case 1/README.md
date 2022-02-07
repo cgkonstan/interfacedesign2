c41875dd5ac757ea13b12d8cd2448e525a5a5cc1
SarahCianciola20 <99061395+SarahCianciola20@users.noreply.github.com>
2022-02-06 22:59:03 -0600

    Project 5
    Final folders made copies for uses

---
+++ Use Case 1/README.md
@@ -0,0 +1,19 @@
+# interfacedesign2
+Interface design 2 Assignment
+
+
+The prototype you created in Part 2 now needs to be a Website.  Iterate (improve upon) your prototype to build the website.   
+
+Reminder of scenarios to take into account:
+
+(1) the company wants only a resume with an optional cover letter,
+
+(2) the company wants a resume and answers to several preliminary questions about the applicant, and
+
+FOR EXTRA CREDIT (15 POINTS)
+
+(3) the company wants a resume and cover letter, answers to several screening questions, and for applicants to take a subject matter quiz to demonstrate their knowledge of the position.
+
+ 
+
+Using HTML, CSS, JS and / or Bootstrap, develop these three use scenarios from your mock-up into a prototype website.  Please develop webpages without using Loren Ipsum.  Develop content that is appropriate to the webpages. 
---
+++ Use Case 1/script.js
@@ -0,0 +1,53 @@
+// Variables
+const prevBtns = document.querySelectorAll(".btn-prev");
+const nextBtns = document.querySelectorAll(".btn-next");
+const progress = document.getElementById("progress");
+const formSteps = document.querySelectorAll(".form-step");
+const progressSteps = document.querySelectorAll(".progress-step");
+
+// Set Up
+let formStepsNum = 0;
+
+// Next Buttons
+nextBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    formStepsNum++;
+    updateFormSteps();
+    updateProgressbar();
+  });
+});
+
+// Prev Buttons
+prevBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    formStepsNum--;
+    updateFormSteps();
+    updateProgressbar();
+  });
+});
+
+// Update Form/Advance Step
+function updateFormSteps() {
+  formSteps.forEach((formStep) => {
+    formStep.classList.contains("form-step-active") &&
+      formStep.classList.remove("form-step-active");
+  });
+
+  formSteps[formStepsNum].classList.add("form-step-active");
+}
+
+// Progress Bar Update
+function updateProgressbar() {
+  progressSteps.forEach((progressStep, idx) => {
+    if (idx < formStepsNum + 1) {
+      progressStep.classList.add("progress-step-active");
+    } else {
+      progressStep.classList.remove("progress-step-active");
+    }
+  });
+// Progress Bar Update
+  const progressActive = document.querySelectorAll(".progress-step-active");
+
+  progress.style.width =
+    ((progressActive.length - 1) / (progressSteps.length - 1)) * 100 + "%";
+}
